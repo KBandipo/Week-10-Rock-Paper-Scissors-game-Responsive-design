789b4af910764685cd81e340a1e997b42c55860a
kennybandy <nekbandy.com>
2023-10-02 13:08:20 +0100

    Use loop 5 rounds

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -41,42 +41,59 @@ let rule = document.querySelector("#rule");
 let gameRuleSection = document.querySelector(".game-rule-section");
 
 /**********************************************************************/
-const userChoice = btns[i].getAttribute("id");
-requestChoice.textContent = `You pick ${userChoice}`;
+let btns = document.querySelectorAll(".btns");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", () => {
+    // if (round >= 5) {
+    //   round = 0;
+    //   computerScore = 0;
+    //   userScore = 0;
+    // } else {
+    //   winnerMessage.classList.add("hide");
 
-let computerChoice = getComputerChoice();
-const computerWins =
-  (computerChoice === "rock" && userChoice === "scissors") ||
-  (computerChoice === "paper" && userChoice === "rock") ||
-  (computerChoice === "scissors" && userChoice === "paper");
-const userWins =
-  (userChoice === "rock" && computerChoice === "scissors") ||
-  (userChoice === "paper" && computerChoice === "rock") ||
-  (userChoice === "scissors" && computerChoice === "paper");
-if (computerChoice === userChoice) {
-  result.textContent = "It's a tie for this round! No scores.";
-} else if (computerWins) {
-  computerScore += 1;
-  compScoreMessage.textContent = `COMPUTER SCORE: ${computerScore}`;
-  result.textContent = `You Lose! ${computerChoice} beats ${userChoice}`;
-} else if (userWins) {
-  userScore += 1;
-  playerScoreMessage.textContent = `PLAYER SCORE: ${userScore}`;
-  result.textContent = `You Win! ${userChoice} beats ${computerChoice}`;
-}
-displayOverallWinnerInfo(displayRounds(round), userScore, computerScore);
-if (round === 1) {
-  computerScore = 0;
-  userScore = 0;
-  playerScoreMessage.textContent = `PLAYER SCORE: ${userScore}`;
-  compScoreMessage.textContent = `COMPUTER SCORE: ${computerScore}`;
-}
-const roundGreaterOrEqualSix = round >= 6;
-if (roundGreaterOrEqualSix) {
-  round = 1;
-} else {
-  round += 1; // same as round++
-}
-if (!roundGreaterOrEqualSix) {
-  winnerMessage.classList.add("hide");
+    //   round += 1; // same as round++
+    // }
+
+    displayRounds(round);
+
+    const userChoice = btns[i].getAttribute("id");
+    requestChoice.textContent = `You pick ${userChoice}`;
+
+    let computerChoice = getComputerChoice();
+    const computerWins =
+      (computerChoice === "rock" && userChoice === "scissors") ||
+      (computerChoice === "paper" && userChoice === "rock") ||
+      (computerChoice === "scissors" && userChoice === "paper");
+    const userWins =
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper");
+    if (computerChoice === userChoice) {
+      result.textContent = "It's a tie for this round! No scores.";
+    } else if (computerWins) {
+      computerScore += 1;
+      compScoreMessage.textContent = `COMPUTER SCORE: ${computerScore}`;
+      result.textContent = `You Lose! ${computerChoice} beats ${userChoice}`;
+    } else if (userWins) {
+      userScore += 1;
+      playerScoreMessage.textContent = `PLAYER SCORE: ${userScore}`;
+      result.textContent = `You Win! ${userChoice} beats ${computerChoice}`;
+    }
+    displayOverallWinnerInfo(displayRounds(round), userScore, computerScore);
+    if (round === 1) {
+      computerScore = 0;
+      userScore = 0;
+      playerScoreMessage.textContent = `PLAYER SCORE: ${userScore}`;
+      compScoreMessage.textContent = `COMPUTER SCORE: ${computerScore}`;
+    }
+    const roundGreaterOrEqualSix = round >= 6;
+    if (roundGreaterOrEqualSix) {
+      round = 1;
+    } else {
+      round += 1; // same as round++
+    }
+    if (!roundGreaterOrEqualSix) {
+      winnerMessage.classList.add("hide");
+    }
+  });
 }
