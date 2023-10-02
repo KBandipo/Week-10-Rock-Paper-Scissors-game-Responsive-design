7de9405ff27139647435436f72e049f97d2f5d97
kennybandy <nekbandy.com>
2023-10-02 12:43:51 +0100

    Link and Assign some  html classes to variabe

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -2,13 +2,12 @@
 let compMessage = document.querySelector(".compMessage");
 compMessage.classList.add("hide");
 
-/************************************************/
+/**********************************************************************/
 function getComputerChoice() {
-  //-Create an array of 'gameChopices'
+  //Create an array of 'gameChopices'
   let gameChoices = ["rock", "paper", "scissors"];
 
   let choice = Math.floor(Math.random() * gameChoices.length);
-  // console.log("Computer picked", gameChoices[choice]);
   if (gameChoices[choice] === "rock") {
     compMessage.textContent = "Computer selects Rock";
     compMessage.classList.remove("hide");
@@ -21,3 +20,24 @@ function getComputerChoice() {
   }
   return gameChoices[choice];
 }
+
+/**********************************************************************/
+
+let result = document.querySelector(".result");
+let winnerMessage = document.querySelector(".winner-message");
+let roundmessage = document.querySelector(".round-count");
+let compScoreMessage = document.querySelector(".comp-score");
+let playerScoreMessage = document.querySelector(".player-score");
+
+let gameContainer = document.querySelector("#game-container");
+let playerSection = document.querySelector(".player-section");
+let computerSection = document.querySelector(".computer-section");
+let display = document.querySelector(".display");
+
+let play = document.querySelector("#play");
+let restart = document.querySelector("#restart");
+let rule = document.querySelector("#rule");
+
+let gameRuleSection = document.querySelector(".game-rule-section");
+
+/**********************************************************************/
