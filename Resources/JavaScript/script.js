9d0141af58d5d61e2b17f68760d8c774dc9c6362
kennybandy <nekbandy.com>
2023-10-02 12:50:19 +0100

    Get the attribute of user input button and use condition on each

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -41,3 +41,42 @@ let rule = document.querySelector("#rule");
 let gameRuleSection = document.querySelector(".game-rule-section");
 
 /**********************************************************************/
+const userChoice = btns[i].getAttribute("id");
+requestChoice.textContent = `You pick ${userChoice}`;
+
+let computerChoice = getComputerChoice();
+const computerWins =
+  (computerChoice === "rock" && userChoice === "scissors") ||
+  (computerChoice === "paper" && userChoice === "rock") ||
+  (computerChoice === "scissors" && userChoice === "paper");
+const userWins =
+  (userChoice === "rock" && computerChoice === "scissors") ||
+  (userChoice === "paper" && computerChoice === "rock") ||
+  (userChoice === "scissors" && computerChoice === "paper");
+if (computerChoice === userChoice) {
+  result.textContent = "It's a tie for this round! No scores.";
+} else if (computerWins) {
+  computerScore += 1;
+  compScoreMessage.textContent = `COMPUTER SCORE: ${computerScore}`;
+  result.textContent = `You Lose! ${computerChoice} beats ${userChoice}`;
+} else if (userWins) {
+  userScore += 1;
+  playerScoreMessage.textContent = `PLAYER SCORE: ${userScore}`;
+  result.textContent = `You Win! ${userChoice} beats ${computerChoice}`;
+}
+displayOverallWinnerInfo(displayRounds(round), userScore, computerScore);
+if (round === 1) {
+  computerScore = 0;
+  userScore = 0;
+  playerScoreMessage.textContent = `PLAYER SCORE: ${userScore}`;
+  compScoreMessage.textContent = `COMPUTER SCORE: ${computerScore}`;
+}
+const roundGreaterOrEqualSix = round >= 6;
+if (roundGreaterOrEqualSix) {
+  round = 1;
+} else {
+  round += 1; // same as round++
+}
+if (!roundGreaterOrEqualSix) {
+  winnerMessage.classList.add("hide");
+}
