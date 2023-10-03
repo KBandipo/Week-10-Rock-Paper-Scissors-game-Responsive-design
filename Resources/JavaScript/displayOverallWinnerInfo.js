da4aa1089779b55a2e56054e3df000112e785ed7
kennybandy <nekbandy.com>
2023-10-03 03:14:52 +0100

    Export function displayOverallWinnerInfo

---
+++ Resources/JavaScript/displayOverallWinnerInfo.js
@@ -0,0 +1,19 @@
+import { winnerMessage, play } from "./variablesContainer.js";
+/***********************************************************************/
+export function displayOverallWinnerInfo(round, userScore, computerScore) {
+  if (round === 6) {
+    winnerMessage.classList.remove("hide");
+
+    if (userScore > computerScore) {
+      winnerMessage.textContent = `You Win! 🏆`;
+    }
+    if (computerScore > userScore) {
+      winnerMessage.textContent = `You Lose!`;
+    }
+    if (computerScore === userScore) {
+      winnerMessage.textContent = `It's a Draw! Play again.`;
+    }
+
+    play.classList.remove("hide");
+  }
+}
